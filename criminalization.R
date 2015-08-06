f7b6846ea5edac7cdfe249fdd2b84aa07524961b
Andrew Heiss <andrew@andrewheiss.com>
2015-08-06 11:17:39 -0400

    Finished criminalization plot

--- criminalization.R
+++ criminalization.R
@@ -21,7 +21,8 @@ theme_blank_map <- function(base_size=12, base_family="Source Sans Pro Light") {
           panel.grid=element_blank(), axis.ticks=element_blank(),
           axis.title=element_blank(), axis.text=element_blank(),
           legend.text=element_text(size=rel(0.7), family="Source Sans Pro Light"),
-          legend.title=element_text(size=rel(0.9), family="Source Sans Pro Semibold"))
+          legend.title=element_text(size=rel(0.9), family="Source Sans Pro Semibold"),
+          strip.text=element_text(size=rel(1.1), family="Source Sans Pro Semibold"))
   ret
 }
 
@@ -31,12 +32,7 @@ theme_blank_map <- function(base_size=12, base_family="Source Sans Pro Light") {
 # -----------
 crim <- read_stata("original_files/kelley_simmons_ajps_2014_replication.dta") %>%
   select(name, year, cowcode, adjbicrimlevel) %>%
-  mutate(id = countrycode(cowcode, "cown", "iso3c"),
-         crim.level = factor(adjbicrimlevel,
-                             levels=c(0, 2),
-                             labels=c("No criminalization", 
-                                      "Full criminalization"),
-                             ordered=TRUE)) %>%
+  mutate(id = countrycode(cowcode, "cown", "iso3c")) %>%
   filter(year %in% c(2001, 2011))
 
 # Load map information
@@ -50,19 +46,29 @@ possible.countries <- expand.grid(id = unique(as.character(countries.ggmap$id)),
                                   year = c(2001, 2011), stringsAsFactors=FALSE)
 
 all.countries <- possible.countries %>% 
-  left_join(crim, by=c("id", "year"))
+  left_join(crim, by=c("id", "year")) %>%
+  mutate(crim.level.num = ifelse(is.na(adjbicrimlevel), -1, adjbicrimlevel),
+         crim.level = factor(crim.level.num,
+                             levels=c(2, 0, -1),
+                             labels=c("Full criminalization    ",
+                                      "No criminalization    ", 
+                                      "No data"),
+                             ordered=TRUE))
 
 
 # -----------
 # Plot data
 # -----------
-# TODO: BW colors
-# TODO: Better facet titles
-# TODO: Nicer legend
-ggplot(all.countries, aes(fill=crim.level)) +
-  geom_map(aes(map_id=id), map=countries.ggmap, size=0) + 
+crim.map <- ggplot(all.countries, aes(fill=crim.level, map_id=id)) +
+  geom_map(map=countries.ggmap) + 
+  # Second layer to add borders and slash-less legend
+  geom_map(map=countries.ggmap, size=0.15, colour="black", show_guide=FALSE) + 
   expand_limits(x=countries.ggmap$long, y=countries.ggmap$lat) + 
   coord_equal() +
   facet_wrap(~ year, ncol=1) + 
-  scale_fill_manual(values=c("#FF4136", "#2ECC40"), na.value="grey90") +
-  theme_blank_map()
+  scale_fill_manual(values=c("grey30", "white", "grey90"), name="") +
+  theme_blank_map() +
+  theme(legend.position="top", legend.key.size=unit(0.5, "lines"),
+        strip.background=element_rect(colour="#FFFFFF", fill="#FFFFFF"))
+crim.map
+ggsave(crim.map, filename="figures/map_criminalization.pdf", device=cairo_pdf)
