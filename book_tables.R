bd2adaea788423067401ae92d1227233dffbe1d7
Andrew Heiss <andrew@andrewheiss.com>
2016-01-20 22:41:42 -0500

    Skeleton for exporting all final tables all at once

---
+++ book_tables.R
@@ -0,0 +1,46 @@
+# Results tables for chapter 2 and 4 - criminalization (2), determinants of documented reaction (4)
+# 5 too - effect of reactions on criminalization
+# 3 = determinants of media coverage
+# 6 = interactions - basic model + interaction with aid (not there), with democracy (there), rest just say nothing there, but don't show tables
+# Chapter 6 pred probs to appendix
+
+
+# Table 1.1: individual and country level drivers of concern for reputation 
+# No data, just a 2×3
+# DONE
+
+#
+# Table 4.1: Individual and country level drivers of concern for reputation
+# No data, just a 2×4
+
+# Table 4.3: Comparison countries
+# List of countries
+
+# Table 4.4: Summary of statistical results
+# TODO: Clean up?
+
+# Table 4.5: Comparison of reaction frequencies, US TIP and US Human Rights Report
+# TODO: Get data for this? - just use existing numbers
+
+
+# Table 5.1: Summary of US influence (tally of tables 5.2 and 5.3)
+# TODO: Clean this up
+# TODO: Add heatmap shading
+
+# Table 5.2: Impact on legislation and implementation in the case studies
+# TODO: Clean this up?
+
+# Table 5.3: Impact on legislation and implementation in the case studies
+# TODO: Clean this up?
+
+
+
+# Table 6.1: Most prominent modifying factors and their presence in the case studies
+# TODO: Clean this up
+
+
+# Table 7.1: Possible motivations or causal pathways: Aggregated evidence
+# TODO: Clean this up
+
+# Table 7.2: Evidence of a relationship between scorecard diplomacy and outcomes
+# TODO: Clean this up
