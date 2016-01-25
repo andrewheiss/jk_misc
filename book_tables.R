d69b30e9f2d29a6fb1b074eb4f0adeae282ffb25
Andrew Heiss <andrew@andrewheiss.com>
2016-01-25 14:17:10 -0500

    Add tables for chapters 1 and 4

--- book_tables.R
+++ book_tables.R
@@ -4,25 +4,80 @@
 # 6 = interactions - basic model + interaction with aid (not there), with democracy (there), rest just say nothing there, but don't show tables
 # Chapter 6 pred probs to appendix
 
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(readxl)
+library(pander)
+library(scales)
+panderOptions('pandoc.binary', '/Users/andrew/.cabal/bin/pandoc')
+panderOptions('table.split.table', Inf)
+panderOptions('table.split.cells', 50)
+panderOptions('keep.line.breaks', TRUE)
+panderOptions('table.style', 'multiline')
+panderOptions('table.alignment.default', 'left')
 
-# Table 1.1: individual and country level drivers of concern for reputation 
-# No data, just a 2×3
-# DONE
+# Locations
+base.folder <- "final_tables"
+all.tables <- file.path(base.folder, "all_tables.xlsx")
 
-#
+
+# -----------
+# Chapter 1
+# -----------
+# Table 1.1: Individual and country level drivers of concern for reputation 
+df.table1.1 <- read_excel(all.tables, sheet="Table 1.1", col_types=NULL) %>%
+  mutate_each(funs(gsub("\r\n", "\\\\ \n", .))) %>%  # Deal with line breaks
+  set_colnames(gsub("BLANK", " ", colnames(.)))  # Rename blank columns
+
+filename = "table1_1_reputation_concern"
+caption = "Table 1.1: Individual- and country-level drivers of concern for reputation"
+table.clean <- pandoc.table.return(df.table1.1, caption=caption)
+cat(table.clean, file=file.path(base.folder, paste0(filename, ".md")))
+
+
+# -----------
+# Chapter 4
+# -----------
 # Table 4.1: Individual and country level drivers of concern for reputation
-# No data, just a 2×4
+filename = "table4_1_reputation_concern"
+caption = "Table 4.1: Individual- and country-level drivers of concern for reputation"
+table.clean <- pandoc.table.return(df.table1.1, caption=caption)
+cat(table.clean, file=file.path(base.folder, paste0(filename, ".md")))
+
+# Table 4.2: Comparison of reaction frequencies, US TIP and US Human Rights Report
+df.table4.5 <- read_excel(all.tables, sheet="Table 4.5") %>%
+  mutate(`Total reports 2001–2009` = comma(`Total reports 2001–2009`),
+         `Percent of reports with documented reaction about TIP` = 
+           percent(`Percent of reports with documented reaction about TIP`))
+
+filename = "table4_2_reactions_tip_hr"
+caption = "Table 4.2: Comparison of reaction frequencies, US TIP and US Human Rights Report"
+table.clean <- pandoc.table.return(df.table4.5, caption=caption)
+cat(table.clean, file=file.path(base.folder, paste0(filename, ".md")))
 
 # Table 4.3: Comparison countries
-# List of countries
+df.table4.3 <- read_excel(all.tables, sheet="Table 4.3", col_types=NULL)
+
+filename = "table4_3_comparison_countries"
+caption = "Table 4.3: Comparison countries"
+table.clean <- pandoc.table.return(df.table4.3, caption=caption)
+cat(table.clean, file=file.path(base.folder, paste0(filename, ".md")))
 
 # Table 4.4: Summary of statistical results
-# TODO: Clean up?
+df.table4.4 <- read_excel(all.tables, sheet="Table 4.4", col_types=NULL) %>%
+  set_colnames(gsub("BLANK", " ", colnames(.)))  # Rename blank columns
 
-# Table 4.5: Comparison of reaction frequencies, US TIP and US Human Rights Report
-# TODO: Get data for this? - just use existing numbers
+filename = "table4_4_stats_summary_results"
+caption = "Table 4.4: Summary of statistical analysis results"
+table.clean <- pandoc.table.return(df.table4.4, caption=caption)
+cat(table.clean, file=file.path(base.folder, paste0(filename, ".md")))
 
 
+# -----------
+# Chapter 5
+# -----------
 # Table 5.1: Summary of US influence (tally of tables 5.2 and 5.3)
 # TODO: Clean this up
 # TODO: Add heatmap shading
@@ -34,11 +89,16 @@
 # TODO: Clean this up?
 
 
-
+# -----------
+# Chapter 6
+# -----------
 # Table 6.1: Most prominent modifying factors and their presence in the case studies
 # TODO: Clean this up
 
 
+# -----------
+# Chapter 7
+# -----------
 # Table 7.1: Possible motivations or causal pathways: Aggregated evidence
 # TODO: Clean this up
 
