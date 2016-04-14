b901d288d1b9ee55def5481b61cdceb93fcbfb26
Andrew Heiss <andrew@andrewheiss.com>
2016-04-13 21:29:33 -0400

    Remove cases of missing data

--- criminalization.R
+++ criminalization.R
@@ -76,6 +76,15 @@ crim.all <- crim %>%
          crim.imputed = retroactively.criminalize(crim.imputed)) %>%
   select(id, year, crim = adjcrimlevel, crim.imputed)
 
+# Manually add data for large missing countries
+updated.countries <- data_frame(id = rep(c("SOM", "SSD", "SRB", "PRK"), each=2),
+                                year = rep(c(2001, 2014), times=4),
+                                crim = NA,
+                                crim.imputed = c(0, 0, 0, 0, 0, 2, 0, 0))
+
+crim.all <- bind_rows(crim.all, updated.countries) %>%
+  mutate(crim.imputed = ifelse(id == "SOM", 0, crim.imputed))
+
 # Load map information
 countries.map <- readOGR("map_data", "ne_110m_admin_0_countries")
 countries.robinson <- spTransform(countries.map, CRS("+proj=robin"))
@@ -89,14 +98,29 @@ possible.countries <- expand.grid(id = unique(as.character(countries.ggmap$id)),
 
 all.countries <- possible.countries %>% 
   left_join(crim.all, by=c("id", "year")) %>%
-  mutate(crim.level.num = ifelse(is.na(crim.imputed), -1, crim.imputed),
+  mutate(crim.level.num = ifelse(is.na(crim.imputed), 0, crim.imputed),
          crim.level = factor(crim.level.num,
-                             levels=c(2, 1, 0, -1),
+                             levels=c(2, 1, 0),
                              labels=c("Full criminalization   ",
                                       "Partial criminalization    ",
-                                      "No criminalization    ", 
-                                      "No data"),
+                                      "No criminalization"),
                              ordered=TRUE))
+
+# Which countries are missing?
+# all.countries %>%
+#   filter(crim.level.num == -1) %>%
+#   distinct(id, year) %>%
+#   mutate(countryname = countrycode(id, "iso3c", "country.name"))
+
+# For the map, we only care about fixing the countries that are visible
+# (sorry tiny islands), so Somalia, South Sudan, Serbia, and North Korea
+#
+# SOM = No in 2001, No in 2014
+# SSD = No (well, non-existent) in 2001, No in 2014
+# SRB = No in 2001, Yes in 2014 (criminalized in 2003)
+# PRK = No in 2001, No in 2014
+#
+# These are added manually above as `updated.countries`
 
 
 # -----------
@@ -109,7 +133,7 @@ crim.map <- ggplot(all.countries, aes(fill=crim.level, map_id=id)) +
   expand_limits(x=countries.ggmap$long, y=countries.ggmap$lat) + 
   coord_equal() +
   facet_wrap(~ year, ncol=1) + 
-  scale_fill_manual(values=c("grey30", "grey60", "white", "grey90"), name="",
+  scale_fill_manual(values=c("grey30", "grey60", "white"), name="",
                     guide=guide_legend(override.aes=list(colour="black", size=0.1))) +
   theme_blank_map(base_size=10) +
   theme(legend.position="top", legend.key.size=unit(0.65, "lines"),
