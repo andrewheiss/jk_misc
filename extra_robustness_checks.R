c1a37f45039d1e1da8ace9c3014905973e44c67f
Andrew Heiss <andrew@andrewheiss.com>
2016-05-11 09:32:57 -0400

    Finish collecting and merging all data for extra robustness checks

--- extra_robustness_checks.R
+++ extra_robustness_checks.R
@@ -1,23 +1,71 @@
 library(dplyr)
+library(tidyr)
 library(readr)
 library(lubridate)
 library(rvest)
 library(countrycode)
-# library(WDI)
-# library(aiddata)
+library(stringr)
+library(WDI)
+library(feather)
 
-# Remove non-aid-eligible countries (get OECD DAC list online) from models with aid in them
-# Aid eligible countries: https://www.oecd.org/dac/stats/documentupload/DAC%20List%20of%20ODA%20Recipients%202014%20final.pdf
+
+# Load original data
+setwd("~/Research/•Judith/jk_misc/chapter_5")
+source("clean_data.R")
+setwd("~/Research/•Judith/jk_misc")
+
+df.full <- df.complete.with.lags.correct
+
+# Remove non-aid-eligible countries from models with aid in them
+# Aid eligible countries: https://www.oecd.org/dac/stats/historyofdaclistsofaidrecipientcountries.htm
 # http://stats.oecd.org/
 #
+oecd.dac <- read_csv("data/oecd_dac_countries.csv") %>%
+  mutate(ccode = countrycode(country, "country.name", "cown"),
+         ccode = ifelse(country == "Serbia", 555, ccode),
+         dac_eligible = TRUE)
+
 
 # Control for US military aid
 # http://www.securityassistance.org/data/country/military/country/1996/2017/is_all/Global
+military.aid <- read_csv("original_files/Military and Police Aid by Country.csv") %>%
+  select(-`year (Year)`) %>%
+  filter(!str_detect(country, "Regional")) %>%
+  filter(!(country %in% c("African Union", "Global",
+                          "Panama Canal Area Military School"))) %>%
+  # TODO: Fix this; right now this just combines Serbia and Montenegro...
+  mutate(country = ifelse(country == "Serbia and Montenegro", "Serbia", country)) %>%
+  mutate(country = countrycode(country, "country.name", "country.name")) %>%
+  gather(year, amount, -country) %>%
+  # There are some duplicate countries, but all only have one amount
+  group_by(country, year) %>%
+  summarise(amount = sum(amount, na.rm=TRUE)) %>%
+  ungroup() %>%
+  mutate(iso3 = countrycode(country, "country.name", "iso3c"),
+         cowcode = countrycode(country, "country.name", "cown"),
+         cowcode = ifelse(country == "Serbia", 555, cowcode),
+         year = as.numeric(year))
 
 
-# Add FDI
+# Add FDI and ODA
 # is there any relationship between tier ratings and FDI? Is FDI less likely in countries with worse ratings, once one controls for other economic and political factors? I suspect not, but it would be nice to test. Similarly for trade. 
 # FDI ~ tier level + controls
+wdi.indicators <- c("BN.KLT.DINV.CD",  # FDI, net inflows (current US$)
+                    "DT.ODA.ALLD.CD")  # Net ODA and official aid received (current US$)
+wdi.raw <- WDI(country="all", wdi.indicators, extra=TRUE, start=1991, end=2016)
+wdi.countries <- countrycode(na.exclude(unique(df.full$cowcode)), "cown", "iso2c")
+
+wdi.clean <- wdi.raw %>%
+  filter(iso2c %in% wdi.countries) %>%
+  rename(fdi = BN.KLT.DINV.CD, oda = DT.ODA.ALLD.CD) %>%
+  mutate(oda.log = sapply(oda, FUN=function(x) ifelse(x < 0, NA, log1p(x))),
+         fdi.log = sapply(fdi, FUN=function(x) ifelse(x < 0, NA, log1p(x)))) %>%
+  mutate(cowcode = countrycode(iso2c, "iso2c", "cown"),
+         cowcode = ifelse(country == "Serbia", 555, cowcode),
+         region = factor(region),  # Get rid of unused levels first
+         region = factor(region, labels =
+                           gsub(" \\(all income levels\\)", "", levels(region)))) %>%
+  select(-c(iso2c, capital, longitude, latitude, lending))
 
 
 # Add BITS
@@ -39,8 +87,8 @@ us.bits <- read_html(bits.url) %>%
          start.date = mdy(start.date),
          start.year = year(start.date),
          bit.partner = countrycode(bit.partner, "country.name", "country.name"),
-         bit.partner.cc = countrycode(bit.partner, "country.name", "cown"),
-         bit.partner.cc = ifelse(bit.partner == "Serbia", 555, bit.partner.cc),
+         bit.partner.cow = countrycode(bit.partner, "country.name", "cown"),
+         bit.partner.cow = ifelse(bit.partner == "Serbia", 555, bit.partner.cow),
          bit.partner.iso = countrycode(bit.partner, "country.name", "iso3c"))
 write_csv(us.bits, path="data/us_bits.csv")
 
@@ -52,49 +100,83 @@ us.bits.panel <- expand.grid(bit.partner = us.bits$bit.partner,
   mutate(has.bit.sig.with.us = year >= sig.year,
          has.bit.start.with.us = year >= start.year)
 
-# US aid as a percent of total aid (get both US and global aid from OECD)
 
+# Imports to the US
 # IMF direction of trade statistics
+# http://data.imf.org/regular.aspx?key=61013712
+imports.us <- read_excel("original_files/External_Trade_by_Counterpart.xls", 
+                         sheet=2, skip=6) %>%
+  select(country = 1, everything()) %>%
+  mutate_each(funs(asdf = ifelse(. == "-" | . == "...", NA, .))) %>%
+  gather(year, amount, -country) %>%
+  mutate(year = as.numeric(year),
+         amount = as.numeric(amount) * 1000000,
+         amount.log = log1p(amount),
+         country = ifelse(country == "Serbia & Montenegro" | 
+                            country == "Serbia & Montenegro n.s.", 
+                          "Serbia", country),
+         country = countrycode(country, "country.name", "country.name")) %>%
+  filter(!is.na(country)) %>%
+  mutate(iso3 = countrycode(country, "country.name", "iso3c"),
+         cowcode = countrycode(country, "country.name", "cown"),
+         cowcode = ifelse(country == "Serbia", 555, cowcode)) %>%
+  arrange(country, year)
+  
 
 # Green book vs. OECD ODA?
 # Green book counts more than OECD does
-# OECD ODA: http://data.worldbank.org/indicator/DT.ODA.ALLD.CD
 # Greenbook: https://catalog.data.gov/dataset/us-overseas-loans-and-grants-greenbook-usaid-1554
 # Foreign aid explorer: https://explorer.usaid.gov/
 # AidData: http://aiddata.org/
+#
+# US aid as a percent of total aid (get both US and global aid from OECD)
+aidata <- read_csv("original_files/AidDataCoreDonorRecipientYear_ResearchRelease_Level1_v3.0.csv") %>%
+  filter(!str_detect(recipient, "Regional")) %>%
+  mutate(recipient.country = countrycode(recipient, "country.name", "country.name"),
+         recipient.country = ifelse(recipient == "Serbia and Montenegro", 
+                                    "Serbia", recipient.country)) %>%
+  filter(!is.na(recipient.country))
+
+aid.all <- aidata %>%
+  group_by(recipient.country, year) %>%
+  summarise(aid.total = sum(commitment_amount_usd_constant_sum, na.rm=TRUE))
+
+aid.us <- aidata %>%
+  filter(donor == "United States") %>%
+  group_by(recipient.country, year) %>%
+  summarise(aid.us = sum(commitment_amount_usd_constant_sum, na.rm=TRUE))
+
+aid.us.total <- aid.all %>%
+  left_join(aid.us, by=c("recipient.country", "year")) %>%
+  mutate(aid.us.total.perc = aid.us / aid.total,
+         aid.total.log = log1p(aid.total),
+         aid.us.log = log1p(aid.us),
+         recipient.iso = countrycode(recipient.country, "country.name", "iso3c"),
+         recipient.cowcode = countrycode(recipient.country, "country.name", "cown"),
+         recipient.cowcode = ifelse(recipient.country == "Serbia",
+                                    as.integer(555), recipient.cowcode)) %>%
+  ungroup()
 
 
-
-
-
-
-
-
-
-# df <- get_aid(donor="US", start=1990, end=2016)
-
-# World Bank World Development Indicators (WDI)
-# http://data.worldbank.org/data-catalog/world-development-indicators
-# Use WDI::WDI() to access the data
-wdi.indicators <- c("BX.KLT.DINV.CD.WD",  # FDI, net inflows (current US$)
-                    "DT.ODA.ALLD.CD")  # Net ODA and official aid received (current US$)
-wdi.raw <- WDI(country="all", wdi.indicators, extra=TRUE, start=1981, end=2016)
-
-wdi.clean <- wdi.raw %>%
-  rename(fdi = BX.KLT.DINV.CD.WD, oda = DT.ODA.ALLD.CD)
-
-# wdi.clean <- wdi.raw %>%
-#   filter(iso2c %in% wdi.countries) %>%
-#   rename(gdpcap = NY.GDP.PCAP.KD, gdp = NY.GDP.MKTP.KD, 
-#          population = SP.POP.TOTL, oda = DT.ODA.ALLD.CD) %>%
-#   mutate(gdpcap.log = log(gdpcap), gdp.log = log(gdp),
-#          population.log = log(population)) %>%
-#   mutate(gdpcap.log = log(gdpcap), gdp.log = log(gdp),
-#          population.log = log(population)) %>%
-#   # Ignore negative values of oda
-#   mutate(oda.log = sapply(oda, FUN=function(x) ifelse(x < 0, NA, log1p(x)))) %>%
-#   mutate(cow = countrycode(iso2c, "iso2c", "cown"),
-#          region = factor(region),  # Get rid of unused levels first
-#          region = factor(region, labels = 
-#                            gsub(" \\(all income levels\\)", "", levels(region)))) %>%
-#   select(-c(iso2c, iso3c, country, capital, longitude, latitude, income, lending))
+robustness.df <- df.full %>% ungroup() %>%
+  expand(cowcode, year) %>%
+  left_join(select(oecd.dac, year, cowcode = ccode, dac_status, dac_abbr, dac_eligible), 
+            by=c("cowcode", "year")) %>%
+  left_join(select(military.aid, year, cowcode, us.military.aid = amount),
+            by=c("cowcode", "year")) %>%
+  left_join(select(wdi.clean, year, cowcode, fdi, oda, fdi.log, oda.log),
+            by=c("cowcode", "year")) %>%
+  left_join(select(us.bits.panel, year, cowcode = bit.partner.cow, 
+                   has.bit.sig.with.us, has.bit.start.with.us),
+            by=c("cowcode", "year")) %>%
+  left_join(select(imports.us, year, cowcode, trade.to.us = amount, 
+                   trade.to.us.log = amount.log),
+            by=c("cowcode", "year")) %>%
+  left_join(select(aid.us.total, year, cowcode = recipient.cowcode, 
+                   aid.total, aid.total.log, aid.us, aid.us.log, 
+                   aid.us.total.perc),
+            by=c("cowcode", "year")) %>%
+  group_by(cowcode) %>%
+  mutate_each(funs(lag = lag))
+  
+write_feather(robustness.df, "data/robustness_df.feather")
