641f9704b9add6c0e9fb24f78bfa8f386ced9ba3
Andrew Heiss <andrew@andrewheiss.com>
2016-06-08 14:23:35 -0400

    Remove investigations

--- crim_activities_cases.R
+++ crim_activities_cases.R
@@ -7,7 +7,7 @@ library(ggplot2)
 
 source("shared_functions.R")
 
-base.folder <- "~/Desktop"
+base.folder <- "final_figures"
 
 cases <- c("ARM", "IDN", "ECU", "MOZ", "KAZ", "ARG", "ISR", 
            "ARE", "NGA", "OMN", "HND", "JPN", "TCD", "ZWE", "MYS")
@@ -17,7 +17,8 @@ crim.activities <- read_csv("original_files/crime_data.csv", na=".") %>%
   select(-contains("original")) %>%
   mutate(iso = countrycode(Country, "country.name", "iso3c"),
          Country = countrycode(iso, "iso3c", "country.name")) %>%
-  gather(activity.type, number, Prosecutions, Convictions, `Opened investigations`)
+  gather(activity.type, number, Prosecutions, Convictions, `Opened investigations`) %>%
+  filter(activity.type != "Opened investigations")
 
 # Criminalization data
 df.cho <- readRDS("data/policy_index.rds") %>%
@@ -38,7 +39,7 @@ fig.crim.activities <- ggplot(crim.activities,
   geom_line() + 
   scale_x_continuous(limits=c(2000, 2015)) + 
   scale_linetype_manual(values=c("dotted", "solid"), guide=FALSE) +
-  scale_colour_manual(values=c("grey80", "grey50", "black"), name=NULL) +
+  scale_colour_manual(values=c("black", "grey65"), name=NULL) +
   labs(x=NULL, y="Count") + 
   facet_wrap(~ Country, scales="free", ncol=3) + 
   theme_clean(10) + 
@@ -48,7 +49,7 @@ fig.crim.activities <- ggplot(crim.activities,
         panel.grid.minor=element_blank(),
         strip.text=element_text(size=rel(0.8)))
 
-filename <- "crim_activities"
+filename <- "figure_cases_crim_activities"
 width <- 4.5
 height <- 6
 ggsave(fig.crim.activities, filename=file.path(base.folder, paste0(filename, ".pdf")), 
